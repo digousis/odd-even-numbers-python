f0152107c16392d0a89090fd109597b4e377856c
digousis <39773090+digousis@users.noreply.github.com>
2018-06-01 16:20:24 +0100

    Add files via upload

---
+++ print_even.py
@@ -0,0 +1,18 @@
+import csv
+import numpy as np
+
+with open("datasets/numbers.csv") as f:
+	reader = csv.reader(f, delimiter=",")
+	num_list = list(reader)
+	
+new_list = list()
+	
+for i in range(0, len(num_list)):
+	new_list.append(num_list[i][0])
+	
+np_list = np.array(new_list)
+np_list = np_list.astype(np.int)
+
+for i in range(0, len(np_list)):
+	if(np_list[i] % 2 == 0):
+		print(np_list[i])
