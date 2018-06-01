df62dd3f9cd36651f825c2c1802bade25877f613
digousis <39773090+digousis@users.noreply.github.com>
2018-06-01 19:22:10 +0100

    Update print_odd.py

--- print_odd.py
+++ print_odd.py
@@ -3,14 +3,14 @@ import numpy as np
 
 with open("numbers.csv") as f:
 	reader = csv.reader(f, delimiter=",")
-	num_list = list(reader)
+	num_list_of_lists = list(reader)
 	
-new_list = list()
+num_list = list()
 	
-for i in range(0, len(num_list)):
-	new_list.append(num_list[i][0])
+for i in range(0, len(num_list_of_lists)):
+	num_list.append(num_list_of_lists[i][0])
 	
-np_list = np.array(new_list)
+np_list = np.array(num_list)
 np_list = np_list.astype(np.int)
 
 for i in range(0, len(np_list)):
